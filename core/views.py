806c3dd89f8b0d38838913d464ac8b7961153369
Natka <natka@HUAWEI_Mate_20_lite-173e9.Charlotte>
2025-04-25 13:35:37 +0200

    Add home and register views with registration form

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils import timezone
+from .models import Task, Note, PomodoroSession, Category, CustomUser
+from .forms import TaskForm, UserRegistrationForm
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'core/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            form = UserRegistrationForm()
+        return render(request, 'core/register.html', {'form': form})
