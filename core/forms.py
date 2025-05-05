0834c778284290c3b39229abec172836b7c304b5
Natka <natka@StaryMacbook.local>
2025-05-05 16:29:40 +0200

    Reset user model and regenerate core migrations

--- core/forms.py
+++ core/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ('username', 'email', 'password1', 'password2')
