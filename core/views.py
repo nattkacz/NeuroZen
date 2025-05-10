31ff2487a6bb87ff012460f695ea115faf50ba22
Natka <natka@StaryMacbook.local>
2025-05-10 13:50:16 +0200

    moved inline styles to style.css, updated dashboard layout and navigation

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from core.forms import UserRegisterForm
-
+from core.models import Task, DailyQuote, MoodEntry
+from datetime import date
 
 
 
@@ -19,5 +21,21 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'core/register.html', {'form': form})
 
+
+@login_required
 def dashboard(request):
-    return render(request, 'core/dashboard.html')
+    user = request.user
+    tasks_today = Task.objects.filter(
+        category__user=user,
+        status='todo',
+        due_date=date.today()
+    )
+    quote = DailyQuote.objects.filter(is_active=True).order_by('?').first()
+    mood_entry = MoodEntry.objects.filter(user=user, date=date.today())\
+                                   .order_by('-time').first()
+
+    return render(request, 'core/dashboard.html', {
+        'tasks_count': tasks_today.count(),
+        'quote': quote,
+        'mood_entry': mood_entry,
+    })
