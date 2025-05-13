ce57920d1ae47570a5a7a0fab605934682ce3be3
Natka <natka@HUAWEI_Mate_20_lite-173e9.Charlotte>
2025-05-13 13:42:52 +0200

    Add TaskForm

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from core.models import Category, Task
 
 User = get_user_model()
 
@@ -10,3 +11,24 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class   TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'due_date', 'priority', 'category', 'tags']
+        widgets = {
+            'due_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'description': forms.Textarea(attrs={'rows': 4}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        if user is not None:
+            self.fields['category'].queryset = Category.objects.filter(is_active=True))
+
+
+
+
+
+
